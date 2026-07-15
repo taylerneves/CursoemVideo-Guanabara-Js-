f6a3d6c3812994700dc57c877c4733961ef33318
taylerneves <taylernevespolletti@gmail.com>
2026-07-15 10:49:06 -0400

    Ajustando as respostas das cores

--- Ex.010 Calculador de idade/script.js
+++ Ex.010 Calculador de idade/script.js
@@ -21,19 +21,13 @@ function verificar() {
     var sexo = "";
     var img = document.createElement("img");
     img.setAttribute("id", "foto");
-    if (idade > 120) {
-        document.body.style.background = "#000";
-        document.querySelector("header").style.color = "#000000";
-        document.querySelector("footer").style.color = "#000000";
-        document.querySelector("section").style.backgroundColor = "#555"; // cinza
-        res.innerHTML = `<strong>💀 Estou te procurando faz tempo...</strong><br>`;
-        img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROE1kCinwnIBffe3Rz-ScrnTgdY5NiMFkeQAxRjTM2Hw&s=10"; // ou um link da web
-        res.appendChild(img);
-        return; // Encerra a função aqui
-    }
 
     if (document.getElementById("mas").checked) {
         sexo = "Masculino";
+        document.body.style.background = "#3857b5";
+        document.querySelector("header").style.color = "#22237c";
+        document.querySelector("footer").style.color = "#0023a1";
+        document.querySelector("section").style.backgroundColor = "#5a91be"; // cinza
 
         if (idade < 12) {
             img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSntMeZSEFj_pg_nktCRPJnMzgWqU2XJsfq10Ff1Hewpw&s=10";
@@ -44,6 +38,10 @@ function verificar() {
         }
     } else {
         sexo = "Feminino";
+        document.body.style.background = "#921c9d";
+        document.querySelector("header").style.color = "#b43ea3";
+        document.querySelector("footer").style.color = "#da5ba1";
+        document.querySelector("section").style.backgroundColor = "#921541"; // cinza
 
         if (idade < 12) {
             img.src = "https://static.vecteezy.com/ti/fotos-gratis/p2/35317676-casual-mulher-branco-criancas-menina-retrato-fofa-menina-mulher-pequeno-pessoa-femea-lindo-crianca-jovem-foto.jpg";
@@ -52,6 +50,16 @@ function verificar() {
         } else {
             img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3yfhxFbauNoWpCzywznftvdw6Cr0-fksvy0ZeLOKVRQ&s=10";
         }
+    }
+    if (idade > 120) {
+        document.body.style.background = "#000";
+        document.querySelector("header").style.color = "#000000";
+        document.querySelector("footer").style.color = "#000000";
+        document.querySelector("section").style.backgroundColor = "#555"; // cinza
+        res.innerHTML = `<strong>💀 Estou te procurando faz tempo...</strong><br>`;
+        img.src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcROE1kCinwnIBffe3Rz-ScrnTgdY5NiMFkeQAxRjTM2Hw&s=10"; // ou um link da web
+        res.appendChild(img);
+        return; // Encerra a função aqui
     }
 
     res.innerHTML = `Detectamos ${sexo} com ${idade} anos.<br>`;
